1a0e27334d11981d8e17edb1206763e0706f7e8f
hp1317 <59136693+hp1317@users.noreply.github.com>
2023-06-04 10:34:35 +0530

    3 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import './style.css';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <div className="bg-dark text-white">
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </div>
+    </>
   );
 }
