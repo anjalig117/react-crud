e0977ad66f708bae5df728c66d84e11e9cee3fdd
anjali <59136693+hp1317@users.noreply.github.com>
2024-06-24 12:48:53 +0530

    displayed the entries data

--- wiki-next/app/page.tsx
+++ wiki-next/app/page.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
 import WikiLogo from "./ui/wiki-logo";
+import { lusitana } from "./ui/fonts";
+import AllEntries from "./ui/entries/allEntries";
 
 export default function Home() {
   return (
     <>
+      <div className="w-full">
+          <div className="flex w-full items-center justify-between">
+            <h1 className={`${lusitana.className} text-2xl`}>
+              Home
+            </h1>
+          </div>
+          <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+
+            </div>
+            <AllEntries/>
+      </div>
     </>
   );
 }
--- wiki-next/app/ui/entries/allEntries.tsx
+++ wiki-next/app/ui/entries/allEntries.tsx
@@ -1,22 +1,50 @@
 import Image from "next/image";
 import { fetchAllEntries } from "@/app/lib/data";
 
-export default async function AllEntries(){
-    const entries = await fetchAllEntries();
+export default async function AllEntries() {
+  const entries = await fetchAllEntries();
 
-    return (
-        <>
-            <div className="mt-6 flow-root">
-                <div className="inline-block min-w-full align-middle">
-                    <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
-                        <ul>
-                        {/* { entries?.map((entry) => (
+  return (
+    <>
+      <div className="mt-6 flow-root">
+        <div className="inline-block min-w-full align-middle">
+          <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+            {/* <ul>
+                            { entries.map((entry) => (
+                                <li key={entry.id}>
+                                    { entry.name }
+                                </li>
+                            )) }
+                        </ul> */}
 
-                        ))} */}
-                        </ul>
+            <ul className="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
+              {entries.map((entry) => (
+                <li key={entry.id} className="pb-3 sm:pb-4 p-2 ">
+                  <div className="flex items-center space-x-4 rtl:space-x-reverse">
+                    <div className="flex-shrink-0">
+                      <Image
+                        className="w-8 h-8 rounded-full"
+                        src={entry.image_url}
+                        alt={entry.name}
+                        width={50}
+                        height={50}
+                      />
                     </div>
-                </div>
-            </div>
-        </>
-    )
+                    <div className="flex-1 min-w-0">
+                      <p className="text-xl font-medium text-sky-500 truncate ">
+                        { entry.name }
+                      </p>
+                      <p className="text-base text-gray-500 truncate dark:text-gray-400">
+                        { entry.content }
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
